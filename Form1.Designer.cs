3aff6d98c135d3e6370181bd4f21b79cb4bc97d0
StarSer <androidemoc@gmail.com>
2025-02-27 14:24:22 -0700

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace SMARTFIT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnConsultar = new System.Windows.Forms.Button();
+            this.BtnInsertarDatos = new System.Windows.Forms.Button();
+            this.BtnEliminar = new System.Windows.Forms.Button();
+            this.BtnCrearBD = new System.Windows.Forms.Button();
+            this.BtnInsercion = new System.Windows.Forms.Button();
+            this.TxtDireccion = new System.Windows.Forms.TextBox();
+            this.TxtHCierre = new System.Windows.Forms.TextBox();
+            this.TxtHApertura = new System.Windows.Forms.TextBox();
+            this.TxtTelefono = new System.Windows.Forms.TextBox();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.SpnCve = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Telefono = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IdGimnasio = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.DG1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DG1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnConsultar
+            // 
+            this.BtnConsultar.BackColor = System.Drawing.Color.Khaki;
+            this.BtnConsultar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnConsultar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnConsultar.Location = new System.Drawing.Point(35, 110);
+            this.BtnConsultar.Name = "BtnConsultar";
+            this.BtnConsultar.Size = new System.Drawing.Size(326, 32);
+            this.BtnConsultar.TabIndex = 26;
+            this.BtnConsultar.Text = "Consultar Datos";
+            this.BtnConsultar.UseVisualStyleBackColor = false;
+            this.BtnConsultar.Click += new System.EventHandler(this.BtnConsultar_Click);
+            // 
+            // BtnInsertarDatos
+            // 
+            this.BtnInsertarDatos.BackColor = System.Drawing.Color.Khaki;
+            this.BtnInsertarDatos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnInsertarDatos.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnInsertarDatos.Location = new System.Drawing.Point(200, 69);
+            this.BtnInsertarDatos.Name = "BtnInsertarDatos";
+            this.BtnInsertarDatos.Size = new System.Drawing.Size(162, 33);
+            this.BtnInsertarDatos.TabIndex = 25;
+            this.BtnInsertarDatos.Text = "Insertar Datos";
+            this.BtnInsertarDatos.UseVisualStyleBackColor = false;
+            this.BtnInsertarDatos.Click += new System.EventHandler(this.BtnInsertarDatos_Click);
+            // 
+            // BtnEliminar
+            // 
+            this.BtnEliminar.BackColor = System.Drawing.Color.Khaki;
+            this.BtnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnEliminar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnEliminar.Location = new System.Drawing.Point(35, 69);
+            this.BtnEliminar.Name = "BtnEliminar";
+            this.BtnEliminar.Size = new System.Drawing.Size(158, 33);
+            this.BtnEliminar.TabIndex = 24;
+            this.BtnEliminar.Text = "Eliminar Base de datos";
+            this.BtnEliminar.UseVisualStyleBackColor = false;
+            this.BtnEliminar.Click += new System.EventHandler(this.BtnEliminar_Click);
+            // 
+            // BtnCrearBD
+            // 
+            this.BtnCrearBD.BackColor = System.Drawing.Color.Khaki;
+            this.BtnCrearBD.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnCrearBD.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCrearBD.Location = new System.Drawing.Point(35, 19);
+            this.BtnCrearBD.Name = "BtnCrearBD";
+            this.BtnCrearBD.Size = new System.Drawing.Size(159, 37);
+            this.BtnCrearBD.TabIndex = 23;
+            this.BtnCrearBD.Text = "Crear Base de Datos";
+            this.BtnCrearBD.UseVisualStyleBackColor = false;
+            this.BtnCrearBD.Click += new System.EventHandler(this.BtnCrearBD_Click);
+            // 
+            // BtnInsercion
+            // 
+            this.BtnInsercion.BackColor = System.Drawing.Color.Khaki;
+            this.BtnInsercion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnInsercion.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnInsercion.Location = new System.Drawing.Point(200, 19);
+            this.BtnInsercion.Name = "BtnInsercion";
+            this.BtnInsercion.Size = new System.Drawing.Size(162, 37);
+            this.BtnInsercion.TabIndex = 22;
+            this.BtnInsercion.Text = "Insertar";
+            this.BtnInsercion.UseVisualStyleBackColor = false;
+            this.BtnInsercion.Click += new System.EventHandler(this.BtnInsercion_Click);
+            // 
+            // TxtDireccion
+            // 
+            this.TxtDireccion.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtDireccion.Location = new System.Drawing.Point(521, 243);
+            this.TxtDireccion.Name = "TxtDireccion";
+            this.TxtDireccion.Size = new System.Drawing.Size(103, 23);
+            this.TxtDireccion.TabIndex = 20;
+            // 
+            // TxtHCierre
+            // 
+            this.TxtHCierre.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtHCierre.Location = new System.Drawing.Point(733, 241);
+            this.TxtHCierre.Name = "TxtHCierre";
+            this.TxtHCierre.Size = new System.Drawing.Size(103, 23);
+            this.TxtHCierre.TabIndex = 19;
+            // 
+            // TxtHApertura
+            // 
+            this.TxtHApertura.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtHApertura.Location = new System.Drawing.Point(733, 203);
+            this.TxtHApertura.Name = "TxtHApertura";
+            this.TxtHApertura.Size = new System.Drawing.Size(103, 23);
+            this.TxtHApertura.TabIndex = 21;
+            // 
+            // TxtTelefono
+            // 
+            this.TxtTelefono.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtTelefono.Location = new System.Drawing.Point(733, 168);
+            this.TxtTelefono.Name = "TxtTelefono";
+            this.TxtTelefono.Size = new System.Drawing.Size(103, 23);
+            this.TxtTelefono.TabIndex = 18;
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtNombre.Location = new System.Drawing.Point(521, 206);
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(103, 23);
+            this.TxtNombre.TabIndex = 17;
+            // 
+            // SpnCve
+            // 
+            this.SpnCve.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SpnCve.Location = new System.Drawing.Point(521, 168);
+            this.SpnCve.Name = "SpnCve";
+            this.SpnCve.Size = new System.Drawing.Size(103, 23);
+            this.SpnCve.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(444, 247);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 15);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Direccion";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(642, 247);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "H. de Cierre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(626, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 15);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "H. de Apertura";
+            // 
+            // Telefono
+            // 
+            this.Telefono.AutoSize = true;
+            this.Telefono.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Telefono.Location = new System.Drawing.Point(664, 171);
+            this.Telefono.Name = "Telefono";
+            this.Telefono.Size = new System.Drawing.Size(63, 15);
+            this.Telefono.TabIndex = 11;
+            this.Telefono.Text = "Telefono";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(456, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 15);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Nombre";
+            // 
+            // IdGimnasio
+            // 
+            this.IdGimnasio.AutoSize = true;
+            this.IdGimnasio.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IdGimnasio.Location = new System.Drawing.Point(435, 171);
+            this.IdGimnasio.Name = "IdGimnasio";
+            this.IdGimnasio.Size = new System.Drawing.Size(80, 15);
+            this.IdGimnasio.TabIndex = 10;
+            this.IdGimnasio.Text = "IdGimnasio";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SMARTFIT.Properties.Resources.LogoSmart;
+            this.pictureBox1.Location = new System.Drawing.Point(336, -7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(182, 122);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 27;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(862, 115);
+            this.panel1.TabIndex = 28;
+            // 
+            // DG1
+            // 
+            this.DG1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DG1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DG1.Location = new System.Drawing.Point(39, 165);
+            this.DG1.Name = "DG1";
+            this.DG1.Size = new System.Drawing.Size(360, 294);
+            this.DG1.TabIndex = 30;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BtnConsultar);
+            this.groupBox1.Controls.Add(this.BtnInsertarDatos);
+            this.groupBox1.Controls.Add(this.BtnEliminar);
+            this.groupBox1.Controls.Add(this.BtnCrearBD);
+            this.groupBox1.Controls.Add(this.BtnInsercion);
+            this.groupBox1.Location = new System.Drawing.Point(438, 298);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(398, 160);
+            this.groupBox1.TabIndex = 31;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(858, 491);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.DG1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.TxtDireccion);
+            this.Controls.Add(this.TxtHCierre);
+            this.Controls.Add(this.TxtHApertura);
+            this.Controls.Add(this.TxtTelefono);
+            this.Controls.Add(this.TxtNombre);
+            this.Controls.Add(this.SpnCve);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Telefono);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.IdGimnasio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DG1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnConsultar;
+        private System.Windows.Forms.Button BtnInsertarDatos;
+        private System.Windows.Forms.Button BtnEliminar;
+        private System.Windows.Forms.Button BtnCrearBD;
+        private System.Windows.Forms.Button BtnInsercion;
+        private System.Windows.Forms.TextBox TxtDireccion;
+        private System.Windows.Forms.TextBox TxtHCierre;
+        private System.Windows.Forms.TextBox TxtHApertura;
+        private System.Windows.Forms.TextBox TxtTelefono;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.TextBox SpnCve;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Telefono;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label IdGimnasio;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView DG1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace SMARTFIT
+{
+    public partial class Form1 : Form
+    {
+        public SqlConnection conexion;
+        public SqlCommand comando;
+        public SqlDataReader Lector;
+        public string q;
+        public string mensaje;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCrearBD_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexion = new SqlConnection(@"Data Source=DESKTOP-GQ6Q9HG\SQLEXPRESS;Initial Catalog=master;Integrated Security=True;");
+                q = "create database SMARTFIT";
+                comando = new SqlCommand(q, conexion);
+                conexion.Open();
+                comando.ExecuteNonQuery();
+                conexion.Close();
+
+                mensaje = "La base de datos fue creada correctamente";
+            }
+            catch (Exception ex)
+            {
+                mensaje = "Ocurrio un error en la creacion de la Base de Datos " + ex.Message;
+            }
+            finally
+            {
+                MessageBox.Show(mensaje);
+            }
+        }
+
+        private void BtnInsercion_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexion = new SqlConnection(@"Data Source=DESKTOP-GQ6Q9HG\SQLEXPRESS;Initial Catalog=SMARTFIT;Integrated Security=True;");
+                q = "use SMARTFIT";
+                comando = new SqlCommand(q, conexion);
+                conexion.Open();
+                comando.ExecuteNonQuery();
+
+                q = "Create Table Gimnasio(Id_gimnasio INT PRIMARY KEY,Nombre VARCHAR(100) NOT NULL," +
+                    "Direccion VARCHAR(255) DEFAULT 'No especificado', Telefono VARCHAR(20) UNIQUE NOT NULL," +
+                    "Horario_apertura TIME CHECK (Horario_apertura >= '05:00')," +
+                    "Horario_cierre TIME CHECK (Horario_cierre <= '23:00'));";
+                comando = new SqlCommand(q, conexion);
+                comando.ExecuteNonQuery();
+
+                mensaje = "Creacion de las Tablas realizada";
+            }
+            catch (System.Exception ex)
+            {
+                mensaje = "Ocurrio un error en la creacion de las tablas " + ex.Message;
+            }
+            finally
+            {
+                MessageBox.Show(mensaje);
+            }
+        }
+
+        private void BtnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (conexion = new SqlConnection(@"Data Source=DESKTOP-GQ6Q9HG\SQLEXPRESS;Initial Catalog=master;Integrated Security=True;"))
+                {
+                    conexion.Open();
+
+                    // Cambiar a modo de usuario único y forzar cierre de conexiones
+                    q = "ALTER DATABASE SMARTFIT SET SINGLE_USER WITH ROLLBACK IMMEDIATE;";
+                    comando = new SqlCommand(q, conexion);
+                    comando.ExecuteNonQuery();
+
+                    // Eliminar la base de datos
+                    q = "DROP DATABASE SMARTFIT;";
+                    comando = new SqlCommand(q, conexion);
+                    comando.ExecuteNonQuery();
+
+                    mensaje = "La base de datos fue eliminada correctamente.";
+                }
+            }
+            catch (System.Exception ex)
+            {
+                mensaje = "Ocurrio un error en la eliminacion de la Base de Datos" + ex.Message;
+            }
+            finally
+            {
+                MessageBox.Show(mensaje);
+            }
+        }
+
+        private void BtnInsertarDatos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexion = new SqlConnection(@"Data Source=DESKTOP-GQ6Q9HG\SQLEXPRESS;Initial Catalog=SMARTFIT;Integrated Security=True;");
+                conexion.Open();
+
+                int IdGimnasio = Convert.ToInt32(SpnCve.Text);
+                string Nombre = TxtNombre.Text;
+                string Telefono = TxtTelefono.Text;
+                string Direccion = TxtDireccion.Text;
+
+                // Para Horario_Apertura, usa TimeSpan directamente
+                TimeSpan Apertura = new TimeSpan(06, 00, 0);  // Asegúrate de que TxtHApertura contenga un formato adecuado como "HH:mm"
+
+                // Para Horario_Cierre, usa TimeSpan directamente
+                TimeSpan Cierre = new TimeSpan(22, 00, 0);  // Aquí puedes mantener la hora predeterminada si es fija, o también puedes usar un campo de texto
+
+                q = "INSERT INTO Gimnasio (Id_Gimnasio, Nombre,Direccion, Telefono, Horario_Apertura, Horario_Cierre) " +
+                "VALUES (@ID, @NOM,@DIR, @TEL, @HAP, @HAC)";
+
+                comando = new SqlCommand(q, conexion);
+
+                comando.Parameters.Clear();
+                comando.Parameters.Add("@ID", SqlDbType.Int).Value = IdGimnasio;
+                comando.Parameters.Add("@NOM", SqlDbType.NVarChar).Value = Nombre;
+                comando.Parameters.Add("@TEL", SqlDbType.NVarChar).Value = Telefono;
+                comando.Parameters.Add("@DIR", SqlDbType.NVarChar).Value = Direccion;
+
+                // Asegúrate de pasar TimeSpan correctamente al parámetro @HAP
+                comando.Parameters.Add("@HAP", SqlDbType.Time).Value = Apertura;
+
+                // Asegúrate de pasar TimeSpan correctamente al parámetro @HAC
+                comando.Parameters.Add("@HAC", SqlDbType.Time).Value = Cierre;
+
+                comando.ExecuteNonQuery();
+                conexion.Close();
+                mensaje = "Datos Almacenados correctamente";
+            }
+            catch (System.Exception ex)
+            {
+                mensaje = "Ocurrio un error en la insersion de datos " + ex.Message;
+            }
+            finally
+            {
+                MessageBox.Show(mensaje);
+            }
+        }
+
+        private void BtnConsultar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexion = new SqlConnection(@"Data Source=DESKTOP-GQ6Q9HG\SQLEXPRESS;Initial Catalog=SMARTFIT;Integrated Security=True;");
+                conexion.Open();
+
+                q = "SELECT * FROM Gimnasio";
+                comando = new SqlCommand(q, conexion);
+                Lector = comando.ExecuteReader();
+
+                // Crear un DataTable para almacenar los datos
+                DataTable dt = new DataTable();
+                dt.Load(Lector);
+
+                // Asignar el DataTable al DataGridView
+                DG1.DataSource = dt;
+
+                conexion.Close();
+                mensaje = "Datos Mostrados Correctamente";
+
+            }
+            catch (System.Exception ex)
+            {
+                mensaje = "Ocurrio un error al mostrar los datos " + ex.Message;
+            }
+            finally
+            {
+                MessageBox.Show(mensaje);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
